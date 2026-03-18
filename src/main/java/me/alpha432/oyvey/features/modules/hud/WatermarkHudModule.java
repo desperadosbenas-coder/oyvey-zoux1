3f9e5936b4e25d255adc888cdb02d292178f36c3
desperadosbenas-coder <desperadosbenas@gmail.com>
2026-03-18 18:36:14 +0200

    Update WatermarkHudModule.java

--- src/main/java/me/alpha432/oyvey/features/modules/hud/WatermarkHudModule.java
+++ src/main/java/me/alpha432/oyvey/features/modules/hud/WatermarkHudModule.java
@@ -7,8 +7,10 @@ import me.alpha432.oyvey.util.BuildConfig;
 import me.alpha432.oyvey.util.TextUtil;
 
 public class WatermarkHudModule extends HudModule {
-    public Setting<String> text = str("Text", BuildConfig.NAME);
+
+    public Setting<String>  text        = str("Text", "zoux1");
     public Setting<Boolean> fullVersion = new Setting<>("FullVersion", false);
+    public Setting<Boolean> showVersion = bool("ShowVersion", true);
 
     public WatermarkHudModule() {
         super("Watermark", "Display watermark", 100, 10);
@@ -20,17 +22,21 @@ public class WatermarkHudModule extends HudModule {
     @Override
     protected void render(Render2DEvent e) {
         super.render(e);
-        String watermarkString = "{global} %s {} %s";
-        if (fullVersion.getValue() && BuildConfig.USING_GIT) {
-            watermarkString += "/" + BuildConfig.BRANCH + "-" + BuildConfig.HASH;
-        }
 
-        e.getContext().drawString(mc.font,
-                TextUtil.text(watermarkString, text.getValue(), BuildConfig.VERSION),
+        String name    = "§b§l" + text.getValue();
+        String version = showVersion.getValue()
+                ? " §8v§7" + BuildConfig.VERSION
+                : "";
+        String branch  = (fullVersion.getValue() && BuildConfig.USING_GIT)
+                ? " §8(" + BuildConfig.BRANCH + "§8-§7" + BuildConfig.HASH + "§8)"
+                : "";
+
+        String watermarkString = name + version + branch;
+
+        e.getContext().drawString(mc.font, watermarkString,
                 (int) getX(), (int) getY(), -1);
 
         setWidth(mc.font.width(watermarkString));
         setHeight(mc.font.lineHeight);
     }
 }
-
